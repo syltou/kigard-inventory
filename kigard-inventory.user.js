16605cd8dc2f6876c7aceb677c2884fbcb77baa7
Sylvain <syltou@protonmail.com>
2023-10-05 23:18:04 +0200

    Add files via upload

---
+++ kigard-inventory.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Kigard Inventory
+// @version      0.1
+// @description  Permet un meilleur usage de l'inventaire et des formules d'artisanat
+// @author       Fergal <ffeerrggaall@gmail.com>
+// @match        https://tournoi.kigard.fr/*
+// @icon         https://tournoi.kigard.fr/images/items/37.gif
+// @grant        none
+// ==/UserScript==
+
