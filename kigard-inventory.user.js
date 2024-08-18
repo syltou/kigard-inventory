ecc2216ab0383fac554368a027da673e268872f5
Sylvain <syltou@protonmail.com>
2024-08-18 13:44:11 +0200

    Update kigard-inventory.user.js

--- kigard-inventory.user.js
+++ kigard-inventory.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name		 Kigard Inventory
-// @version	  1.5.15
+// @version	  1.6
 // @description  Permet un meilleur usage de l'inventaire et des formules d'artisanat, et rajoute un radar dans la vue
 // @author	   Fergal <ffeerrggaall@gmail.com>
 // @match		https://tournoi.kigard.fr/*
